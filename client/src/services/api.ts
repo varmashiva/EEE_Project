0a342cb0b455961141bf9138e9c791fa0256035a
Shiva Varma <shivavarma@Shivas-MacBook-Pro.local>
2026-02-06 17:04:06 +0530

    added

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "http://34.224.99.183:3000",
+  baseURL: "http://100.26.21.48:3000",
 });
 
 export default api;
